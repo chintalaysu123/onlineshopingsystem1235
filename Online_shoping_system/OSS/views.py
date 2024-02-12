245f26bcf8669c2888672e0e874fa68dbe0417d2
shank <shankar.2021csu@gmail.com>
2024-02-11 20:01:13 -0500

    The project aims to design and implement an online shopping web application which provides multiple items for customers throughout the United States. Our team’s target is to guarantee that users could always get responses even with large requests from users. Which means we need to asynchronously handle requests from peak time to leisure time.

--- Online_shoping_system/OSS/views.py
+++ Online_shoping_system/OSS/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'home.html',{'name':'Shanker'})
+
+def data(request):
+
+    return render(request,'result.html')
